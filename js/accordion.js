2788e476f7ba0fc7a28a9a6862f32cf8348d5b1c
Oleksandr Goncharenko <goncharenko.oleksandr@gmail.com>
2022-03-14 23:24:13 +0200

    14.03.2022 19th day of the war

--- js/accordion.js
+++ js/accordion.js
@@ -2,12 +2,14 @@ let accordions = document.getElementsByClassName('accordion__wrapper');
 let accordionsButtons = document.getElementsByClassName('line__button');
 
 let accordionAllArr = Array.from(accordions);
-let BtnAllArr = Array.from(accordionsButtons);
+let btnAllArr = Array.from(accordionsButtons);
 
-BtnAllArr.forEach((btnItem, btnIndex) => {
+btnAllArr.forEach((btnItem, btnIndex) => {
+    console.log(btnItem, btnIndex)
     btnItem.addEventListener('click', () => {
         btnItem.firstElementChild.classList.toggle('arrow--down');
         accordionAllArr.forEach((accItem, accIndex) => {
+            console.log(accItem, accIndex)
             if (btnIndex === accIndex) {
                 accItem.classList.toggle('accordion__wrapper--non-active');
             } 
--- js/index.js
+++ js/index.js
@@ -8,12 +8,10 @@ function headerSrcollController() {
     if (window.scrollY > 10) {
         headerInner.classList.add('header_fixed-large');
         headerLine.classList.add('header__line--visible');
-    }
-    else if (window.scrollY <= 9) {
+    } else if (window.scrollY <= 9) {
         headerInner.classList.remove('header_fixed-large');
         headerLine.classList.remove('header__line--visible');
     }
-
 }
 
 let windowSize = window.matchMedia('(max-width: 990px)');
@@ -37,11 +35,37 @@ navToggle.addEventListener('click', navBtnController);
 
 function navBtnController(e){
     if (e.target.className == 'nav__toggle' || e.target.className == 'toggle__item') {
-        console.log(e.target.className);
         navToggle.classList.toggle('active');
-        if(navWindow.style.display != 'block') {
+        if(navWindow.style.display !== 'block') {
         navWindow.style.display = 'block';
-        } else if(navWindow.style.display = 'block')
+        } else if(navWindow.style.display == 'block')
         (navWindow.style.display = '');
     } else {}
 }
+
+let ageVerificatorBtns = document.querySelector('.ageverificator__btns');
+let ageVerificatorFailed = document.querySelector('.ageverificator__failed');
+let wrapperAgeVerificator = document.querySelector('.wrapper__ageverificator');
+let ageVerificatorText = document.querySelector('.ageverificator__text');
+ageVerificatorBtns.addEventListener('click', ageVerificatorBtnsController);
+
+function ageVerificatorBtnsController(e) {
+    if (e.target.className == 'btn-no') {
+        ageVerificatorFailed.innerHTML = 'К сожалению, доступ на сайт для вас закрыт';
+        ageVerificatorBtns.remove();
+        ageVerificatorText.remove();
+    } else if (e.target.className == 'btn-yes') {
+        localStorage.setItem('ageVerefication', true);
+        wrapperAgeVerificator.style.display = 'none';
+    } else {}
+}
+
+const cheackAge = () => {
+    console.log(localStorage.getItem('ageVerefication'))
+    if (localStorage.getItem('ageVerefication')) return;
+
+    wrapperAgeVerificator.classList.add('wrapper__ageverificator--show');
+}
+
+cheackAge();
+
