356a6156f7b1052b236ad7ad1630cf12540a0649
Oleksandr Goncharenko <goncharenko.oleksandr@gmail.com>
2022-03-18 12:24:28 +0200

    18.03.2022

--- js/accordion.js
+++ js/accordion.js
@@ -1,18 +1,23 @@
 let accordions = document.getElementsByClassName('accordion__wrapper');
 let accordionsButtons = document.getElementsByClassName('line__button');
+let lineContainers = document.getElementsByClassName('line__container');
 
+let lineContainerAll = Array.from(lineContainers);
 let accordionAllArr = Array.from(accordions);
 let btnAllArr = Array.from(accordionsButtons);
 
-btnAllArr.forEach((btnItem, btnIndex) => {
-    console.log(btnItem, btnIndex)
-    btnItem.addEventListener('click', () => {
-        btnItem.firstElementChild.classList.toggle('arrow--down');
-        accordionAllArr.forEach((accItem, accIndex) => {
-            console.log(accItem, accIndex)
-            if (btnIndex === accIndex) {
-                accItem.classList.toggle('accordion__wrapper--non-active');
-            } 
+lineContainerAll.forEach((lineItem, lineIndex) => {
+    lineItem.addEventListener('click', () => {
+        btnAllArr.forEach((btnItem, btnIndex) => {
+            if(lineIndex == btnIndex + 1) {
+                btnItem.firstElementChild.classList.toggle('arrow--down');
+                btnItem.classList.toggle('line__button_active');
+                accordionAllArr.forEach((accItem, accIndex) => {
+                    if (btnIndex === accIndex) {
+                        accItem.classList.toggle('accordion__wrapper--non-active');
+                    } else {}
+                })
+            } else{}
         })
     })
-});
+})
